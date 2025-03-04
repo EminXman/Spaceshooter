26e5a630f13c4e922a6faac7cfebbf6e2c2288aa
Emin Gredelj <emin.gredelj@learnet.se>
2025-03-04 14:00:36 +0100

    bakgrund och wasd kontrolls

--- main.py
+++ main.py
@@ -24,12 +24,20 @@ pygame.display.set_caption("Space Shooter")
 spelet_körs = True
 
 original_bild = pygame.image.load("assets/sprites/SpaceShip.png")
+backgrund_mörkblå = pygame.image.load("assets/backgrounds/bg.png")
+backgrund_stjärnor = pygame.image.load("assets/backgrounds/stars-A.png")
+sprite_jetstråle = pygame.image.load("assets/sprites/fire.png") 
+
+backgrund_y = 0
 
 sprite_spelare = pygame.transform.scale(original_bild, (original_bild.get_width() // 2, original_bild.get_height() // 2))
 
 spelare_x = skärmens_bredd // 2 - 120
 spelare_y = skärmens_höjd - 200
-spelarens_hastighet = 1
+spelarens_hastighet = 2
+
+jetstråle_x = spelare_x + 40
+jetstråle_y = spelare_y + 100
 
 while (spelet_körs == True):
 
@@ -39,19 +47,27 @@ while (spelet_körs == True):
 
         if event.type == pygame.QUIT:
             spelet_körs = False
-        
-        skärm.blit(sprite_spelare, (spelare_x, spelare_y))
 
-        pygame.display.update()
+    skärm.blit(backgrund_mörkblå, (0, 0))
+    skärm.blit(backgrund_stjärnor, (0, backgrund_y))
+    skärm.blit(backgrund_stjärnor, (0, backgrund_y - skärmens_höjd))
+
+    backgrund_y += 2
+
+    if backgrund_y > skärmens_höjd:
+        backgrund_y = 0
+
+    skärm.blit(sprite_spelare, (spelare_x, spelare_y))
+    pygame.display.update()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and spelare_x > 0:
+    if keys[pygame.K_a] and spelare_x > 0:
         spelare_x = spelare_x - spelarens_hastighet
-    if keys[pygame.K_RIGHT] and spelare_x < skärmens_bredd - sprite_spelare.get_width():
+    if keys[pygame.K_d] and spelare_x < skärmens_bredd - sprite_spelare.get_width():
         spelare_x = spelare_x + spelarens_hastighet
-    if keys[pygame.K_UP] and spelare_y > 0:
+    if keys[pygame.K_w] and spelare_y > 0:
         spelare_y = spelare_y - spelarens_hastighet
-    if keys[pygame.K_DOWN] and spelare_y < skärmens_höjd - sprite_spelare.get_width() + 26:
+    if keys[pygame.K_s] and spelare_y < skärmens_höjd - sprite_spelare.get_width() + 26:
         spelare_y = spelare_y + spelarens_hastighet
 
 pygame.quit()
