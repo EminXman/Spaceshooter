6ac85c4ccfc6ed17caf0cfe9a97252a55c34c910
Emin Gredelj <emin.gredelj@learnet.se>
2025-03-25 13:55:43 +0100

    Fixat skotten och lagt till asteroider

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
-# Skriv din kod här för att skapa spelet! Följ dessa steg:
-'''
-Steg 1 - Skapa en skärm och rita ett skepp
-Steg 2 - Lägga till en scrollande stjärnbakgrund
-Steg 3 - Sätt jetmotorer på rymdskeppet
-Steg 4 - Gör så rymdskeppet kan skjuta
-Steg 5 - Slumpa fram Asteroider 
-Steg 6 - Detektera kollisioner mellan rymdskeppet och asteroiden
-Steg 7 - Skapa explosionseffekten (samt lär dig partikeleffekter)
-Steg 8 - Gör så att rymdskeppet kan explodera i kollision med asteroiden
-Steg 9 - Gör så att rymdskeppet kan skjuta ner asteroider
-Steg 10 - Lägg till musik och ljudeffekter
-'''
-
 import pygame
+import random
 
+# Skärmstorlek
 skärmens_bredd = 800
 skärmens_höjd = 600
 
+# Skapa skärmen
 skärm = pygame.display.set_mode((skärmens_bredd, skärmens_höjd))
-
 pygame.display.set_caption("Space Shooter")
 
+# Spelstatus
 spelet_körs = True
 
+# Ladda bilder
 original_bild = pygame.image.load("assets/sprites/SpaceShip.png")
 backgrund_mörkblå = pygame.image.load("assets/backgrounds/bg.png")
 backgrund_stjärnor = pygame.image.load("assets/backgrounds/stars-A.png")
-sprite_jetstråle = pygame.image.load("assets/sprites/fire.png") 
+sprite_jetstråle = pygame.image.load("assets/sprites/fire.png")
 sprite_skott = pygame.image.load("assets/sprites/bullet.png")
+sprite_ateroid_liten = pygame.image.load("assets/sprites/small-A.png")
 
+# Lista för skott
 skott_lista = []
 
+# Bakgrundsposition
 backgrund_y = 0
 
+# Skala spelarens sprite
 sprite_spelare = pygame.transform.scale(original_bild, (original_bild.get_width() // 2, original_bild.get_height() // 2))
 
+# Spelarens position och hastighet
 spelare_x = skärmens_bredd // 2 - 120
 spelare_y = skärmens_höjd - 200
 spelarens_hastighet = 3
 jetstråle_x = spelare_x + 25
 jetstråle_y = spelare_y - 25
 
-class Skott:
+# Variabel för att hålla reda på tiden mellan skott
+skott_räknare = 0
+skott_frekvens = 20  # Justera detta värde för att ändra tiden mellan skott
 
+# Variabel för att hålla reda på tiden mellan asteroider
+asteroid_räknare = 0
+asteroid_frekvens = 50  # Justera detta värde för att ändra tiden mellan asteroider
+
+# Klass för skott
+class Skott:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -50,17 +53,33 @@ class Skott:
         self.bild = sprite_skott
 
     def flytta(self):
-        self.y = self.y - self.hastighet
+        self.y -= self.hastighet
 
     def rita(self, skärm):
         skärm.blit(self.bild, (self.x, self.y))
 
-while (spelet_körs == True):
+# Klass för små asteroider
+class AsteroidLiten:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.hastighet = 4
+        self.bild = sprite_ateroid_liten
 
+    def flytta(self):
+        self.y += self.hastighet
+
+    def rita(self, skärm):
+        skärm.blit(self.bild, (self.x, self.y))
+
+# Lista för asteroider
+asteroid_liten_lista = []
+
+# Huvudloopen för spelet
+while spelet_körs:
     skärm.fill((0, 0, 30))
 
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             spelet_körs = False
 
@@ -69,42 +88,51 @@ while (spelet_körs == True):
     skärm.blit(backgrund_stjärnor, (0, backgrund_y - skärmens_höjd))
 
     backgrund_y += 2
-
     if backgrund_y > skärmens_höjd:
         backgrund_y = 0
 
     skärm.blit(sprite_spelare, (spelare_x, spelare_y))
-
     skärm.blit(sprite_jetstråle, (jetstråle_x, jetstråle_y))
 
     jetstråle_x = spelare_x + (sprite_spelare.get_width() // 2) - (sprite_jetstråle.get_width() // 2)
     jetstråle_y = spelare_y + sprite_spelare.get_height() - (sprite_jetstråle.get_height() // 2)
 
-    pygame.display.update()
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] and spelare_x > 0:
-        spelare_x = spelare_x - spelarens_hastighet
-        jetstråle_x = jetstråle_x - spelarens_hastighet
+        spelare_x -= spelarens_hastighet
+        jetstråle_x -= spelarens_hastighet
     if keys[pygame.K_d] and spelare_x < skärmens_bredd - sprite_spelare.get_width():
-        spelare_x = spelare_x + spelarens_hastighet
-        jetstråle_x = jetstråle_x + spelarens_hastighet
+        spelare_x += spelarens_hastighet
+        jetstråle_x += spelarens_hastighet
     if keys[pygame.K_w] and spelare_y > 0:
-        spelare_y = spelare_y - spelarens_hastighet
-        jetstråle_y = jetstråle_y - spelarens_hastighet
+        spelare_y -= spelarens_hastighet
+        jetstråle_y -= spelarens_hastighet
     if keys[pygame.K_s] and spelare_y < skärmens_höjd - sprite_spelare.get_width() + 26:
-        spelare_y = spelare_y + spelarens_hastighet
-        jetstråle_y = jetstråle_y + spelarens_hastighet
-    if keys[pygame.K_SPACE]:
-        print(type(Skott))
+        spelare_y += spelarens_hastighet
+        jetstråle_y += spelarens_hastighet
+    if keys[pygame.K_SPACE] and skott_räknare >= skott_frekvens:
         skott_lista.append(Skott(spelare_x + 20, spelare_y))
+        skott_räknare = 0
 
     for skott_obj in reversed(skott_lista):
         skott_obj.flytta()
         skott_obj.rita(skärm)
-    
-    if skott_obj.y < -100:
-        skott_lista.remove(skott_obj)
+        if skott_obj.y < -100:
+            skott_lista.remove(skott_obj)
+
+    skott_räknare += 1  # Öka räknaren varje gång spelet uppdateras
+
+    if asteroid_räknare >= asteroid_frekvens:
+        asteroid_liten_lista.append(AsteroidLiten(random.randint(0, skärmens_bredd - sprite_ateroid_liten.get_width()), 100))
+        asteroid_räknare = 0
+
+    for asteroid_liten in reversed(asteroid_liten_lista):
+        asteroid_liten.flytta()
+        asteroid_liten.rita(skärm)
+        if asteroid_liten.y > skärmens_höjd:
+            asteroid_liten_lista.remove(asteroid_liten)
+
+    asteroid_räknare += 1  # Öka räknaren varje gång spelet uppdateras
 
     pygame.display.update()
 
